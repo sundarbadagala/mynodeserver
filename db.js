3cd5d29bf3fc2e56ffca71c5879b8491fef4db99
sundar <sundar.badagala@gmail.com>
2023-10-04 21:24:34 +0530

    env file update

--- db.js
+++ db.js
@@ -4,7 +4,9 @@ module.exports = async () => {
   try {
     await mongoose
       .set("strictQuery", false)
-      .connect("mongodb+srv://admin:admin@cluster0.9hnzoza.mongodb.net/?retryWrites=true&w=majority")
+      .connect(
+        `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.9hnzoza.mongodb.net/?retryWrites=true&w=majority`
+      )
       .then(() => console.log("db connected..."));
   } catch (error) {
     console.log(`db not connected ${error}`);
