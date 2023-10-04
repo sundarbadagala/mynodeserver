5e8b440315822d4f4c49b2ba58348fe1caeaf36a
sundar <sundar.badagala@gmail.com>
2023-10-04 20:03:48 +0530

    profile password update

--- routes/profile.js
+++ routes/profile.js
@@ -1,18 +1,33 @@
-const express = require('express')
-const middleware = require('../middleware/jwt.middleware')
-const User = require('../model/register')
+const express = require("express");
+const middleware = require("../middleware/jwt.middleware");
+const User = require("../model/register");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', middleware, async (req, res)=>{
-    console.log(req.user)
-    try {
-        const user = await User.findById(req.user.id)
-        if(!user) return res.status(400).json('User Not Found')
-        return res.status(200).json(user)
-    } catch (error) {
-        return res.status(500).json('Server Error')
-    }
-})
+router.get("/", middleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+    if (!user) return res.status(400).json("User Not Found");
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json("Server Error");
+  }
+});
 
-module.exports = router
+router.put("/", middleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+    const { password } = req.body;
+    if (!password) return res.status(400).json("Invalid Psassword");
+    if (password === user.password)
+      return res.status(400).json("Same Password");
+    user.password = password;
+    user.confirmpassword = password;
+    await user.save();
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json("Server Error");
+  }
+});
+
+module.exports = router;
